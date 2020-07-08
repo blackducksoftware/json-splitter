7b07bce20a2f9632023d8d6623eeb3bd45aeebb9
Murat Kumykov <mkumykov@blackducksoftware.com>
2020-07-08 10:33:54 -0400

    Added code to ensure that chunks will have scan size below the BD limit.

--- src/split_scan_graph_json.py
+++ src/split_scan_graph_json.py
@@ -6,11 +6,12 @@ Created on Dec 13, 2018
 
 import json
 from warnings import catch_warnings
-from sys import argv
+from sys import argv, version_info, exit
+from collections import OrderedDict
 
 def load_json_file(filename):
     with open(filename) as f:
-        data = json.load(f)
+        data = json.load(f, object_pairs_hook=OrderedDict)
     return data
 
 def write_json_file(data, filename):
@@ -43,21 +44,28 @@ def fix_node_list(data, alldata):
 #
 # Entry Point
 #
+print (version_info.major)
+if version_info.major < 3:
+    print ("Python 3 is required, exiting")
+    exit (1)
 
-print (len(argv))
+print ("Runnign Python {}.{}.{}".format(version_info.major, version_info.minor, version_info.micro))
 print ("processing %s " % argv[1])
 
 scanData  = load_json_file(argv[1])
 dataLength = len(scanData['scanNodeList'])
+scanSize = sum(node['size'] for node in scanData['scanNodeList'] if node['uri'].startswith("file://"))
 
 print ("Number of scan entries %s" % dataLength)
+print ("Total size of the scan %s" % scanSize)
 
 print( scanData.keys())
 
-splitStep = 200000
-
-
-print (splitStep)
+# Establishing two limiting factors, number of node list entries and size of the scan
+# Size of the scan includes file objects only, files within archive files are not counted.
+#
+maxNodeEntries = 200000
+maxScanSize = 4500000000
 
 scanData['project'] = scanData['project'] + "-more"
 scanName = scanData['name']
@@ -69,18 +77,42 @@ base = scanNodeList[0]
 
 print(base)
 
-for i in range(0, dataLength, splitStep):
-    nodeData = scanNodeList[i:i+splitStep]
+# Computing split points for the file
+#
+scanChunkSize = 0
+scanChunkNodes = 0
+splitAt = [0]
+for i in range(0, dataLength-1):
+    if scanChunkSize + scanNodeList[i+1]['size'] > maxScanSize or scanChunkNodes + 1 > maxNodeEntries:
+        scanChunkSize = 0
+        scanChunkNodes = 0
+        splitAt.append(i) 
+    if scanNodeList[i]['uri'].startswith('file://'):
+        scanChunkSize = scanChunkSize + scanNodeList[i]['size']
+    scanChunkNodes += 1
+    
+# Create array of split points shifting by one position
+splitTo = splitAt[1:]
+splitTo.append(None)
+
+print(splitAt)
+print(splitTo)
+
+
+# Splitting and writing the chunks
+#
+
+for i in range(len(splitAt)):
+    print ("Processing range {}, {}".format(splitAt[i], splitTo[i]))
+#for i in range(0, dataLength, maxNodeEntries):
+    nodeData = scanNodeList[splitAt[i]:splitTo[i]]
     if i > 0:
         nodeData.insert(0,base)
     # scanData['baseDir'] = baseDir + "-" + str(i)
     scanData['scanNodeList'] = nodeData
-    scanData['name'] = scanName + "-" + str(i)
-    filename = argv[1] + "-" + str(i) + '.json'
+    scanData['name'] = scanName + "-" + str(splitAt[i])
+    filename = argv[1] + "-" + str(splitAt[i]) + '.json'
     write_json_file(scanData, filename)
     scanData.pop('scanNodeList')
-    
-    
-    
 
-
+print("Done.")
